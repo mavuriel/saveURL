144651eabfc1a9d5f523086819d058faf67bed04
mavuriel <urieljtenorio@gmail.com>
2021-09-24 20:43:23 -0500

    show table data

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ import './style.css'
 
 const inNombre = document.querySelector('#nombreinput')
 const inURL = document.querySelector('#urlinput')
-// const tReg = document.querySelector('#registros')
-
+const tReg = document.querySelector('#registros')
 const btnEnviar = document.querySelector('#guardar')
 
-// eslint-disable-next-line prefer-const
-let paginas = []
+const paginas = []
 
 btnEnviar.addEventListener('click', () => {
   const nombre = inNombre.value
   const url = inURL.value
-  // const btnEliminar = "<input id='eliminar' type='button' value=" + nombre + '>'
-  console.log('hola')
   paginas.push({ nombre, url })
-  console.log(paginas.length)
+  tReg.innerHTML = ''
+  agregar()
 })
 
-console.log(paginas.length)
-// paginas.forEach(e => console.log(e.nombre))
+const agregar = () => {
+  paginas.forEach((e) => {
+    tReg.insertAdjacentHTML('beforeend',
+      '<tr>' +
+      '<td>' + e.nombre + '</td>' +
+      '<td><a href="' + e.url + '">' + e.nombre + '</a></td>' +
+      '<td>eliminar</td>' +
+      '</tr>')
+  })
+}
