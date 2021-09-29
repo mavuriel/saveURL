358a0bc6be0b454d77cbcfce71be5f5eb8b6a807
mavuriel <urieljtenorio@gmail.com>
2021-09-28 19:34:41 -0500

    add update action - hour text

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
 import './style.css'
 import { supabase } from './supabase'
 
+const inId = document.querySelector('#idinput')
 const inNombre = document.querySelector('#nombreinput')
 const inURL = document.querySelector('#urlinput')
 const tReg = document.querySelector('#registros')
-const btnEnviar = document.querySelector('#guardar')
+const btnGuardar = document.querySelector('#btnguardar')
+const timeP = document.querySelector('.time')
 
-const date = Date.now()
-console.log(Date.parse(date))
+const wTime = new Date().getHours()
+
+const textTime = wTime >= 19
+  ? 'buenas noches (¯﹃¯)'
+  : wTime >= 12 && wTime <= 18
+    ? 'buenas tardes ( ͡ᵔ ᴗ ͡ᵔ)'
+    : 'buenos dias ᕦ(ò_óˇ)ᕤ'
+
+timeP.insertAdjacentText('afterbegin', textTime)
 
 const allData = async () => {
   const { data: urlData, error } = await supabase
     .from('urlData')
     .select('*')
+    .order('id', { ascending: true })
   clearIn()
   urlData.forEach(e => {
     tReg.insertAdjacentHTML('beforeend',
     `<tr>
       <td>${e.name_url}</td>
       <td><a href="${e.url_link}">ir a ${e.name_url}</a></td>
-      <td><button value='${e.id}' class='eliminar'>Eliminar</button></td>
+      <td>
+        <button value='${e.id}' class='eliminar'>Eliminar</button>
+        <button value='${e.id}' class='actualizar'>Actualizar</button>
+      </td>
     </tr>`
     )
   })
+  console.log({ urlData, error })
 }
 
 allData()
 
-btnEnviar.addEventListener('click', () => {
+btnGuardar.addEventListener('click', () => {
+  const idV = inId.value
   const nombreV = inNombre.value
   const urlV = inURL.value
-
-  insertURL(nombreV, urlV)
-  allData()
+  if (btnGuardar.value === 'insertar') {
+    insertURL(nombreV, urlV)
+    allData()
+  } else if (btnGuardar.value === 'actualizar') {
+    updateURL(idV, nombreV, urlV)
+    btnGuardar.value = 'insertar'
+    allData()
+  }
 })
 
 tReg.addEventListener('click', (e) => {
@@ -41,6 +61,9 @@ tReg.addEventListener('click', (e) => {
       const id = parseInt(e.target.value)
       deleteURl(id)
       allData()
+    } else if (e.target.classList[0] === 'actualizar') {
+      const id = parseInt(e.target.value)
+      oneReg(id)
     }
   }
 })
@@ -55,8 +78,6 @@ const insertURL = async (nombre, url) => {
       }
     ])
   console.log({ data, error })
-
-  clearIn()
 }
 
 const deleteURl = async (id) => {
@@ -68,11 +89,34 @@ const deleteURl = async (id) => {
   console.log({ data, error })
 }
 
+const updateURL = async (id, nombre, url) => {
+  const { data, error } = await supabase
+    .from('urlData')
+    .update({
+      name_url: nombre,
+      url_link: url
+    })
+    .eq('id', id)
+  console.log({ data, error })
+}
+
+const oneReg = async (id) => {
+  const { data: urlData, error } = await supabase
+    .from('urlData')
+    .select('*')
+    .eq('id', id)
+
+  urlData.forEach(e => {
+    inId.value = e.id
+    inNombre.value = e.name_url
+    inURL.value = e.url_link
+  })
+  btnGuardar.value = 'actualizar'
+  console.log({ urlData, error })
+}
+
 const clearIn = () => {
   inNombre.value = ''
   inURL.value = ''
   tReg.innerHTML = ''
 }
-/*
-  TODO: modificar columnas
-*/
